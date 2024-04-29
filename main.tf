3e7c7a16ac1302e31d49969e7aa36e82a7303139
jadedjelly <kellyjd83@gmail.com>
2024-04-29 17:16:17 +0100

    updated tf files

--- main.tf
+++ main.tf
@@ -1,15 +1,35 @@
 provider "aws" {
   region = "eu-west-1"
-  access_key = ""
-  secret_key = ""
 }
 
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"
+}
+variable "vpc_cidr_block" {
+  description = "vpc cidr block"
+}
+
+
 resource "aws_vpc" "development-vpc"{
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name: "development"
+  }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
-  cidr_block = "10.0.10.0/24"
+  cidr_block = var.subnet_cidr_block
   availability_zone = "eu-west-1a"
+  tags = {
+    Name: "subnet-1-dev"
+  }
+}
+
+output "dev-vpc-id" {
+  value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet-id" {
+  value = aws_subnet.dev-subnet-1.id
 }
