eac3e4f28dc80606bb93f37601d3ef38b6efef90
jadedjelly <kellyjd83@gmail.com>
2024-04-30 20:26:40 +0100

    updated demo files

--- main.tf
+++ main.tf
@@ -1,27 +1,101 @@
-provider "aws" {}
-
-variable cidr_blocks {
-  description = "cidr blocks and name tags for vpc and subnets"
-  type = list(object({
-    cidr_block = string
-    name = string
-  })) 
+provider "aws" {
+  region = "eu-central-1"
 }
 
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
 variable avail_zone {}
+variable env_prefix {}
+variable "my_ip" {}
+variable "instance_size" {}
 
 resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name: var.cidr_blocks[0].name
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.cidr_blocks[1].cidr_block
+  cidr_block = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
-    Name: var.cidr_blocks[1].name
+    Name: "${var.env_prefix}-subnet-1"
   }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-internet-gateway.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-route-table"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-internet-gateway" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name : "${var.env_prefix}-internet-gateway"
+  }
+}
+
+resource "aws_route_table_association" "myapp-route-table-association" {
+  route_table_id = aws_route_table.myapp-route-table.id
+  subnet_id = aws_subnet.myapp-subnet-1.id
+}
+
+resource "aws_security_group" "myapp-securityGroup" {
+  name = "myapp-securityGroup"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags = {
+    Name : "${var.env_prefix}-securityGroup"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# output "test_ami" {
+#   value = data.aws_ami.latest-amazon-linux-image
+# }
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_size
 }
